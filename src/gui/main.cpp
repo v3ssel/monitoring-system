eab60d454c3041d29f5b58b5b80d1f15589ef27d
v3ssel <v3ssels@pm.me>
2024-02-01 02:09:23 +0300

    add console to gui

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,7 +1,22 @@
 #include <QApplication>
 #include "MonitoringSystemWindow.h"
 
+#if defined __WIN32__
+    #include <windows.h>
+    #include <cstdio>
+#endif
+
 int main(int argc, char *argv[]) {
+    #if defined __WIN32__
+        FreeConsole();
+        AllocConsole();
+        AttachConsole(GetCurrentProcessId());
+
+        std::freopen("CON", "w", stdout);
+        std::freopen("CON", "w", stderr);
+        std::freopen("CON", "r", stdin);
+    #endif
+
     QApplication a(argc, argv);
     s21::MonitoringSystemWindow w;
     w.show();
